6e300aea0d6c547c2e580f803a09697097cf26fa
Gunnar Hellekson <gunnar@hellekson.com>
2014-11-08 11:46:46 -0600

    adding UTF8 encoding explicitly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require_relative '../lib/EventCentral.rb'
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 RSpec.configure do |config|
   config.formatter = "documentation"
 end
